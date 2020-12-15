5e6ab2f53a6e1b50e71842503c124bd11a5b694c
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-12-14 21:07:13 -0700

    initialized backend package.json, installed passport and brcrypt

--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 // parsing middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
 
 app.listen(PORT, () => {
