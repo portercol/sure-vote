828744bda9fea4a5716c14cb1868faefecdcbb6c
cleminator <alvinclemens@gmail.com>
2020-12-14 18:28:03 -0700

    made server file and server.js

---
+++ Server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = require('./routes');
+const app = express();
+require('./config/db')();
+
+const PORT = process.env.PORT || 5000;
+
+// parsing middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(PORT, () => {
+  console.log('app running on PORT: ' + PORT);
+});
