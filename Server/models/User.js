b345f8cb7a344ba63fba6465ec6ca8f739f4cd2c
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-12-15 20:11:00 -0700

    added validation to email and password

--- Server/models/User.js
+++ Server/models/User.js
@@ -1,9 +1,25 @@
 const mongoose = require("mongoose");
+import { isEmail } from "validator";
 
 //initialize mongoose schema
 const Schema = mongoose.Schema;
 
 //create mongoose Schema for user model
 const UserSchema = new Schema({
-
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [isEmail, "Email is invalid"]
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: [8, "Password needs to be at least 8 characters long"]
+    }
 });
+
+//initialize user model
+const User = mongoose.model("User", UserSchema);
+
+module.exports = { User };
