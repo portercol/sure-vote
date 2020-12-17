68b9fcd7c98dab020ed30cd8ab0e2932b7990374
cleminator <alvinclemens@gmail.com>
2020-12-17 08:48:28 -0700

    moved my camera tests from an HTML sandbox to react and added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,20 @@ import './App.css';
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import Homepage from './pages/Homepage'
+import Camera from './pages/cam.test'
 
 function App() {
   return (
     <Router>
-    {/* <Navbar/> */}
-    <div className="App">
-      <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route exact path='/signup' component={SignUp} />
-        <Route exact path='/profile' component={Profile} />
-      </Switch>
-    </div>
+      {/* <Navbar/> */}
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route exact path='/signup' component={SignUp} />
+          <Route exact path='/profile' component={Profile} />
+          <Route exact path='/cam' component={Camera} />
+        </Switch>
+      </div>
     </Router>
   );
 }
