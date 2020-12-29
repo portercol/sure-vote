599106f05c6c9a3698a6ad83a21dda8741e8a824
cleminator <alvinclemens@gmail.com>
2020-12-28 18:33:10 -0700

    working on agatha

--- Server/server.js
+++ Server/server.js
@@ -15,7 +15,7 @@ const router = express.Router();
 app.use(logger("dev"));
 
 const PORT = process.env.PORT || 5000;
-// API key from Azure - will be hiding the key but the .env file is not woking 
+// API key from Azure - will be hiding the key but the .env file is not woking right now and i would like to move on
 const ApiKey = "7181d339df244c32b23cc0858cc5d58b"
 // Azure endpoint URL - Face API
 const AzureEndpoint = "https://westus.api.cognitive.microsoft.com/face/v1.0"
