88fe0ad4abfab36ba9bac1f63c3dfd496e4ef4e7
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2021-01-01 15:18:37 -0700

    installed uuid packaged, initialized in api.routes.js

--- Server/routes/api.routes.js
+++ Server/routes/api.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
+const { v4: uuid } = require("uuid");
 
 const User = require("../models/User");
 
