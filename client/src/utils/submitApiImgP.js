ed7527132b742ac5e5757cf70db8fb4d18d947bf
cleminator <alvinclemens@gmail.com>
2021-01-02 16:57:13 -0700

    working on seperating the functions

---
+++ client/src/utils/submitApiImgP.js
@@ -0,0 +1,14 @@
+import ApiCalls from "./ApiCalls";
+
+
+export function submitToAgatha(incomingValues, image, cb) {
+    var reader = new FileReader();
+    reader.onload = () => {
+        var api = new ApiCalls();
+        api.PostImage(api.personPictureEndPoint(incomingValues.personGroupId, incomingValues.person.personId), reader.result)
+            .then(cb)
+
+
+    };
+    reader.readAsArrayBuffer(image);
+};
