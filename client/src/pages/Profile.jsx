4f9fe4a7c934388b528961a0c1972a126de47f19
Collin Porter <colls.potta@gmail.com>
2021-01-01 22:05:54 -0700

    made homepage, signup, signin and profile pages all responsive with changes to styling

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -16,29 +16,29 @@ const Profile = () => {
   return (
     <>
       <Navbar />
-      <Jumbotron className="main-jumbotron">
-          <Card >
-            <Card.Img variant="top" src={landLady} />
-            <Card.Body>
-              <Card.Title>Landlady Lady</Card.Title>
-              <Card.Text>
-                If you're behind on your rent I'll work with you.
-              </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroupItem>Joe Shmoe<span></span></ListGroupItem>
-              <ListGroupItem>Password:<span id="password-span"></span></ListGroupItem>
-              <ListGroupItem>Street: <span id="street-span">123 Main St.</span></ListGroupItem>
-              <ListGroupItem>City: <span id="city-span">Anytown</span></ListGroupItem>
-              <ListGroupItem>State: <span id="state-span">Utah</span></ListGroupItem>
-            </ListGroup>
-            <Card.Body>
-              <Button variant="dark" type="submit">
-                Update Profile
-              </Button>
-            </Card.Body>
-          </Card>
-          </Jumbotron>
+      <div class="col d-flex justify-content-center">
+        <Card className="mainCard" style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={landLady} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              If you're behind on your rent I'll work with you.
+            </Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>Joe Shmoe<span></span></ListGroupItem>
+            <ListGroupItem>Password:<span id="password-span"></span></ListGroupItem>
+            <ListGroupItem>Street: <span id="street-span">123 Main St.</span></ListGroupItem>
+            <ListGroupItem>City: <span id="city-span">Anytown</span></ListGroupItem>
+            <ListGroupItem>State: <span id="state-span">Utah</span></ListGroupItem>
+          </ListGroup>
+          <Card.Body>
+            <Button variant="dark" type="submit">
+              Update Profile
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
     </>
   );
 };
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,57 +1,57 @@
 // import Rreact, elements from React-Bootstrap, SignUp.css
 import React from "react";
-import { Jumbotron, Form, Button, ButtonGroup, Row, Col } from "react-bootstrap";
+import { Button, ButtonGroup, Col, Container, Form, Jumbotron } from "react-bootstrap";
 import './SignIn.css'
 
 // create functional component to hold sign up page data
 const SignIn = () => {
 
-// create function for submit button 'onclick'
-const submitBtn = () => {
-  console.log("submitted sign in credentials");
-}
+  // create function for submit button 'onclick'
+  const submitBtn = () => {
+    console.log("submitted sign in credentials");
+  }
 
   return (
     <>
-      <Row>
-        <Col xs lg={3}></Col>
-        <Col xs lg={6}>
-          <Jumbotron id="signIn-jumbotron">
-            <h1 id="signIn">Sign In</h1>
-            <hr/>
-            <p>Please Enter Your Credentials</p>
-            <hr/>
-            <Form>
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridEmail">
-                  <Form.Label></Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
+      <Container id="main-container">
+        <Jumbotron id="signIn-jumbotron">
+          <h1 id="signIn">Sign In</h1>
+          <hr />
+          <p>Please Enter Your Credentials</p>
+          <hr />
+          <Form>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label></Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridPassword">
-                  <Form.Label></Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-              </Form.Row>
-            </Form>
-            
-            <div id="userSignIn">
-              <ButtonGroup size="lg" className="mr-3">
-              <Button href="/" onClick={submitBtn()} variant="dark" 
-              type="submit" id='right-button'>Go Back</Button>
-              </ButtonGroup>
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label></Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+                <Form.Text className="text-muted">
+                  We'll never share your password with anyone else.
+                </Form.Text>
+              </Form.Group>
+            </Form.Row>
+          </Form>
 
-              <ButtonGroup size="lg" className="mr-3">
+          <div id="userSignIn">
+            <ButtonGroup size="lg" className="mr-3">
+              <Button href="/" onClick={submitBtn()} variant="dark"
+                type="submit" id='right-button'>Go Back</Button>
+            </ButtonGroup>
+
+            <ButtonGroup size="lg" className="mr-3">
               <Button href="/profile" onClick={submitBtn()} variant="dark"
-              type="submit" id='left-button'>Sign In</Button>
-              </ButtonGroup>
-
-            </div>
-            
-          </Jumbotron>
-        </Col>
-        <Col xs lg={3}></Col>
-      </Row>
+                type="submit" id='left-button'>Sign In</Button>
+            </ButtonGroup>
+          </div>
+        </Jumbotron>
+      </Container>
     </>
   );
 };
