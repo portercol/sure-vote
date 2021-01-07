34994340655aebad26b449b0c23842f27c705090
LayneHansen <layne.d.hansen@gmail.com>
2021-01-06 21:05:30 -0700

    wrote function and route request to pull data from db onto profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 // import React, elements from React-Bootstrap, Navbar.jsx and img from assets
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Card,
   Container,
@@ -13,68 +13,72 @@ import {
 import Navbar from "../components/Navbar.jsx";
 import landLady from "../assets/landlady.jpg";
 import "../pages/Profile.css";
+import axios from 'axios';
 
-// create functional component to hold profile data
 const Profile = () => {
-  // useEffect(() => {
-  //   async function fetchProfileData(){
-  //     try {
-  //       const response = await fetch("/api/signup");
-  //       const json = await response.json();
-  //       console.log({ json });
-  //     } catch (err) {
-  //       console.log({ err });
-  //     }
-  //   }
 
-  //   fetchProfileData();
-  // }, []);
+  // const [profileData, setProfileData] = useState();
+
+  const getProfile = () => {
+    axios
+      .get('/api/profile', 
+      {
+        _id: "5ff67acffb1185391c11fdfb",
+      }).then((res) => {
+        console.log(res)
+      }).catch(err => {
+        console.log(err);
+      })
+  }
+
+  getProfile();
 
   return (
     <>
       <Navbar />
-        <Jumbotron>
-          <Container>
-            <Row>
-              <Col xs lg={4}></Col>
-            <Col xs lg={4}>    
-          <Card className="mainCard" style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={landLady} />
-              <Card.Body>
-                <Card.Title>{}</Card.Title>
-                {/* <Card.Text>
-              If you're behind on your rent I'll work with you.
-            </Card.Text> */}
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroupItem>
-                  Joe Shmoe<span></span>
-                </ListGroupItem>
-                {/* <ListGroupItem>Password:<span id="password-span">{uuid}</span></ListGroupItem> */}
-                <ListGroupItem>
-                  Street: <span id="street-span">123 Main St.</span>
-                </ListGroupItem>
-                <ListGroupItem>
-                  City: <span id="city-span">Anytown</span>
-                </ListGroupItem>
-                <ListGroupItem>
-                  State: <span id="state-span">Utah</span>
-                </ListGroupItem>
-              </ListGroup>
-              <Card.Body>
-                <Button variant="dark" type="submit">
-                  Update Profile
-                </Button>
-              </Card.Body>
-            </Card>
-              </Col> 
-              <Col xs lg={4}></Col>
-              </Row>
-          </Container>
-        </Jumbotron>
+      <Jumbotron>
+        <Container>
+          <Row>
+            <Col xs lg={4}></Col>
+            <Col xs lg={4}>
+              <Card className="mainCard" style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={landLady} />
+                <Card.Body>
+                  <Card.Title>{}</Card.Title>
+                  {/* <Card.Text>
+                 If you're behind on your rent I'll work with you.
+                 </Card.Text> */}
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem>
+                    <span></span>
+                  </ListGroupItem>
+                  {/* <ListGroupItem>Password:<span id="password-span">{uuid}</span></ListGroupItem> */}
+                  <ListGroupItem>
+                    Street: <span id="street-span">123 Main St.</span>
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    City: <span id="city-span">Anytown</span>
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    State: <span id="state-span">Utah</span>
+                  </ListGroupItem>
+                </ListGroup>
+                <Card.Body>
+                  <Button variant="dark" type="submit">
+                    Update Profile
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs lg={4}></Col>
+          </Row>
+        </Container>
+      </Jumbotron>
     </>
   );
-};
+
+                }
 
 // export component from Profile.jsx
 export default Profile;
