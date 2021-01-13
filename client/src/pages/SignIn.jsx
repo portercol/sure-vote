04a9bbb6c7586be54a73f96c5938dff29a94cba2
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2021-01-12 22:56:18 -0700

    more work with routes

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -40,8 +40,8 @@ const SignIn = () => {
                 dispatch({ type: "UPDATE_PERSONID", payload: res.data.personId });
 
                 console.log("Redirect to profile");
-                // redirectHandler();
-                <Redirect to="/profile" />
+                redirectHandler();
+
                 console.log("Redirected to profile");
             }).catch(err => {
                 console.log(err);
@@ -49,88 +49,92 @@ const SignIn = () => {
         }
     }
 
-    // const redirectHandler = () => {
-    //     setRedirect(true);
-    //     console.log("redirect handler: ", redirect);
-    //     renderRedirect();
-    // }
+    const redirectHandler = () => {
+        setRedirect(true);
+        console.log("redirect handler: ", redirect);
+        // renderRedirect();
+    }
 
     // const renderRedirect = () => {
     //     if (redirect) {
     //         console.log("render handler: ", redirect);
     //         return <Redirect to="/cam2" />
     //     }
-}
+    // }
 
-return (
-    <>
-        <Container id="main-container">
-            <Jumbotron id="main-jumbotron">
-                <h1>Sign In</h1>
-                <hr />
-                <Form id="signIn-form">
-                    <Form.Group controlId="exampleForm.ControlInput1">
-                        <Form.Label></Form.Label>
-                        <Form.Control
-                            type="email"
-                            placeholder="Email"
-                            onChange={e => setUsernameLogin(e.target.value)}
-                        />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone.
+    if (redirect) {
+        return <Redirect to="/profile" />
+    }
+
+    return (
+        <>
+            <Container id="main-container">
+                <Jumbotron id="main-jumbotron">
+                    <h1>Sign In</h1>
+                    <hr />
+                    <Form id="signIn-form">
+                        <Form.Group controlId="exampleForm.ControlInput1">
+                            <Form.Label></Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Email"
+                                onChange={e => setUsernameLogin(e.target.value)}
+                            />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone.
               </Form.Text>
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlTextarea1">
-                        <Form.Label></Form.Label>
-                        <Form.Control
-                            type="password"
-                            placeholder="Password"
-                            onChange={e => setPasswordLogin(e.target.value)}
-                        />
-                        <Form.Text className="text-muted">
-                            We'll never share your password with anyone.
+                        </Form.Group>
+                        <Form.Group controlId="exampleForm.ControlTextarea1">
+                            <Form.Label></Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                onChange={e => setPasswordLogin(e.target.value)}
+                            />
+                            <Form.Text className="text-muted">
+                                We'll never share your password with anyone.
               </Form.Text>
-                    </Form.Group>
-                </Form>
-                <br />
-                <ButtonGroup size="lg" className="mr-3">
-                    <Button href='/' onClick={() => { submitBtn() }} variant="dark"
-                        type="submit" id='right-button'>Go Back</Button>
-                </ButtonGroup>
+                        </Form.Group>
+                    </Form>
+                    <br />
+                    <ButtonGroup size="lg" className="mr-3">
+                        <Button href='/' onClick={() => { submitBtn() }} variant="dark"
+                            type="submit" id='right-button'>Go Back</Button>
+                    </ButtonGroup>
 
-                <ButtonGroup size="lg" className="mr-3">
-                    <Button onClick={() => { submitBtn() }} variant="dark"
-                        type="submit" id='left-button'>Sign In</Button>
-                </ButtonGroup>
-                <hr></hr>
-                <h5>Protected Routes</h5>
-                <Link to="/cam2">Add Person (Cam 2)</Link>
-                <br></br>
-                <Link to="/profile">Profile</Link>
-                <br></br>
-                <Link to="/cam3">Identify Person (Cam 3)</Link>
-                <br></br>
-                <Link to="/contact">Contact</Link>
-                <br></br>
-                <Link to="/face">App2 (Face)</Link>
-                <br></br>
-                <Link to="/election">Election</Link>
-                <br></br>
-                <Link to="/ballot">Ballot</Link>
-                <br></br>
-                <Link to="/vote">Vote</Link>
-                <hr></hr>
-                <h5>Unprotected Routes</h5>
-                <Link to="/">Homepage</Link>
-                <br></br>
-                <Link to="/signup">Signup</Link>
-                <br></br>
-                <Link to="/signin">Signin</Link>
+                    <ButtonGroup size="lg" className="mr-3">
+                        <Button onClick={() => { submitBtn() }} variant="dark"
+                            type="submit" id='left-button'>Sign In</Button>
+                    </ButtonGroup>
+                    <hr></hr>
+                    <h5>Protected Routes</h5>
+                    <Link to="/cam2">Add Person (Cam 2)</Link>
+                    <br></br>
+                    <Link to="/profile">Profile</Link>
+                    <br></br>
+                    <Link to="/cam3">Identify Person (Cam 3)</Link>
+                    <br></br>
+                    <Link to="/contact">Contact</Link>
+                    <br></br>
+                    <Link to="/face">App2 (Face)</Link>
+                    <br></br>
+                    <Link to="/election">Election</Link>
+                    <br></br>
+                    <Link to="/ballot">Ballot</Link>
+                    <br></br>
+                    <Link to="/vote">Vote</Link>
+                    <hr></hr>
+                    <h5>Unprotected Routes</h5>
+                    <Link to="/">Homepage</Link>
+                    <br></br>
+                    <Link to="/signup">Signup</Link>
+                    <br></br>
+                    <Link to="/signin">Signin</Link>
 
-            </Jumbotron>
-        </Container>
-    </>
-);
+                </Jumbotron>
+            </Container>
+        </>
+    );
 };
 
 // export component from SignUp.jsx
