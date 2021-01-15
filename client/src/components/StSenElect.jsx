b4991fae8b1b9f24fa989a57593606e76547a4a6
LayneHansen <layne.d.hansen@gmail.com>
2021-01-15 15:42:27 -0700

    update

--- client/src/components/StSenElect.jsx
+++ client/src/components/StSenElect.jsx
@@ -46,7 +46,7 @@ const StSenElect = () => {
     const selectedElection = electionList.find(currentElection => currentElection.office === "Utah State Senator")
     const userVoting = userId.id
     console.log(userVoting);
-    axios.post('/api/vote', { candidate: selectedCandidate._id, election: selectedElection._id, user: userId.id })
+    axios.post('/api/vote', { candidate: selectedCandidate._id, election: selectedElection._id })
       .then((res) => {
         // console.log(res.data)
         setVoted(true)
